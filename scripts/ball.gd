20e9819724e627c5231f7c527d8736d04a6b2dd9
Stefan Smith <stefanosmith2005@gmail.com>
2025-10-07 17:32:30 -0500

    There are now goal areas

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,8 +3,20 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 var velocity = Vector2(500, -500)
+var player_goals = 0
+var opponent_goals = 0
 
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info: # This just detects if there is a collision.
 		velocity = velocity.bounce(collision_info.get_normal())
+
+# This code will execute when the player concedes a goal.
+func _on_player_goal_body_entered(body: Node2D) -> void:
+	opponent_goals += 1
+	print(opponent_goals)
+
+
+func _on_enemy_goal_body_entered(body: Node2D) -> void:
+	player_goals += 1
+	print(player_goals)
