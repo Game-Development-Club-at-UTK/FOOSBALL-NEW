4ff5452bd0d17ecce0f0e8861ceb30a5c709fec3
Henry-Hill1 <henrymh2005@gmail.com>
2025-11-03 11:45:29 -0500

    controller support
    added controller support for movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,9 @@
 extends StaticBody2D
 
 var velocity = Vector2(0,0)
+var max_speed = 500
+var acceleration = 60
+var friction = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,11 +13,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("s"):
-		position.y += 500 * delta
-	if Input.is_action_pressed("w"):
-		position.y -= 500 * delta
+	if Input.is_action_pressed("s") or Input.is_action_pressed("w"): # if either input is hit 
+		position.y += 500 * delta * (Input.get_action_strength("s") - Input.get_action_strength("w"))
 
-func _physics_process(delta: float) -> void:
+
+func _physics_process(delta):
 	move_and_collide(velocity * delta) # Keeps the player from leaving the borders.
 	
