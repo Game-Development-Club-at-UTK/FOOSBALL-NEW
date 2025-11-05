30b194c97dd59a9de6f572b2e48356b490d0e048
Henry-Hill1 <henrymh2005@gmail.com>
2025-11-05 17:21:53 -0500

    controls with variable stick

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,13 +10,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	velocity.x = 0
-	if Input.is_action_pressed("s"):
-		velocity.y = 500
-	elif Input.is_action_pressed("w"):
-		velocity.y = -500
-	else:
-		velocity.y = 0
+	if Input.is_action_pressed("s") or Input.is_action_pressed("w"): # if either input is hit 
+		position.y += 500 * _delta * (Input.get_action_strength("s") - Input.get_action_strength("w"))
 
 func _physics_process(delta: float) -> void:
 	move_and_collide(velocity * delta) # Keeps the player from leaving the borders.
